9a8bd474ad33e51f1c35d55c16def517bb9b7ce3
rylex00 <63154652+rylex00@users.noreply.github.com>
2020-08-27 13:06:43 +0300

    Create wordlist-maker.py

---
+++ wordlist-maker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet RYLEX")
+os.system("figlet WORDLIST ARACI")
+
+print("""
+Wordlist Oluşturma aracına Hoş geldin...                   >CODER BY RYLEX
+""")
+
+minimum=input("minimum karakter sayısı : ")
+maximum=input("maximum karakter sayısı : ")
+karakter=input("kullanılacak karakterleri giriniz : ")
+kayityeri=input("kayıt yerinin yolunu giriniz : ")
+
+os.system("chrunch "+minimum+" "+maximum+" "+karakter+" -o "+kayityeri)
+
+print("İşlemler başarıyla tamamlandı...")
